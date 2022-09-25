0fe8e2d5a0d311c8f02c72bf381e092b5d6f0016
LumAuro <33938566+LumAuro@users.noreply.github.com>
2022-09-24 20:41:51 -0500

    Delete appvigiaprecios directory

--- appvigiaprecios/src/main/java/appvigia/codigo/Model/dao/IUsuariosDao.java
+++
@@ -1,9 +0,0 @@
-package appvigia.codigo.Model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import appvigia.codigo.Model.entities.Usuarios;
-
-public interface IUsuariosDao extends CrudRepository<Usuarios, Integer> {
-
-}
