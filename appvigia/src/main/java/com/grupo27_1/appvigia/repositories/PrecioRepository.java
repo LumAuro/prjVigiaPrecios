c72c6dc75a6122570fb09eed0c3bb585d0b32fbf
LumAuro <33938566+LumAuro@users.noreply.github.com>
2022-09-24 20:42:15 -0500

    Delete appvigia directory

--- appvigia/src/main/java/com/grupo27_1/appvigia/repositories/PrecioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo27_1.appvigia.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grupo27_1.appvigia.entities.Precio;
-
-public interface PrecioRepository extends JpaRepository<Precio, Float> {
-
-}
--- appvigia/src/main/java/com/grupo27_1/appvigia/repositories/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo27_1.appvigia.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grupo27_1.appvigia.entities.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Float> {
-
-}
