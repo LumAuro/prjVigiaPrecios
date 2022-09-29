587a8ebbedeaf57060e2cd6f000bdce49791f665
LumAuro <33938566+LumAuro@users.noreply.github.com>
2022-09-28 19:38:29 -0500

    limpieza

--- appvigiaprecios/src/main/java/appvigia/codigo/Model/dao/IProductosDao.java
+++
@@ -1,9 +0,0 @@
-package appvigia.codigo.Model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import appvigia.codigo.Model.entities.Productos;
-
-public interface IProductosDao extends CrudRepository<Productos, Long> {
-
-}
--- appvigiaprecios/src/main/java/appvigia/codigo/Model/dao/ITiendasDao.java
+++
@@ -1,9 +0,0 @@
-package appvigia.codigo.Model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import appvigia.codigo.Model.entities.Tiendas;
-
-public interface ITiendasDao extends CrudRepository<Tiendas, Long> {
-
-}
